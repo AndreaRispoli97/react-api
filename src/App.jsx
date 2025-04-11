485979ff39ff2f9fc25fa35c98c2e5c5dd6d6c98
AndreaRispoli97 <Andrea9715@outlook.it>
2025-04-11 15:03:42 +0200

    feat:added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 
-function App() {
 
+function App() {
+  const [actress, setActress] = useState([])
+  function fetchActress() {
+    axios.get('https://freetestapi.com/api/v1/actresses')
+      .then((res) => setActress(res.data))
+  }
 
   return (
     <>
-      <div>Ciao</div>
+      <div>
+        <button onClick={fetchActress}>Carica Todos</button>
+        <ul>
+          {actress.map((act) => (
+            <li key={act.id}>{act.name}</li>
+          ))}
+        </ul></div>
     </>
   )
 }
